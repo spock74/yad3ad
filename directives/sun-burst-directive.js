1553d1e82f3110adea11fe0ebd7e3e4ef8dfe432
Jose Moraes <elvano@gmail.com>
2015-04-05 11:32:23 -0300

    organizing code into folders

--- directives/sun-burst-directive.js
+++ directives/sun-burst-directive.js
@@ -64,7 +64,7 @@ angular.module('d3AngularApp')
 
           // Use d3.text and d3.csv.parseRows so that we do not need to have a header
           // row, and can receive the csv as an array of arrays.
-          d3.text("visit-sequences.csv", function(text) {
+          d3.text("data/visit-sequences.csv", function(text) {
             var csv = d3.csv.parseRows(text);
             var json = buildHierarchy(csv);
             createVisualization(json);
