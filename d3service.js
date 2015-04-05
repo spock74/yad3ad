83a99b37a348ff771c0cf50338f6beaf08175d6a
Jose Moraes <elvano@gmail.com>
2015-04-04 22:18:55 -0300

    d3 injected as an angular service. sunBurst vis being implemented as an angular directive

--- d3service.js
+++ d3service.js
@@ -12,7 +12,16 @@ angular.module('d3serviceMod', [])
   var scriptTag = $document[0].createElement('script');
   scriptTag.type = 'text/javascript'; 
   scriptTag.async = true;
-  scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+  //
+  // Zi
+  //
+  // scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+  // Load d3 not from web, but from a local bower_components
+  //TODO: put some logic to test path to d3
+  scriptTag.src = 'bower_components/d3/d3.min.js';
+  //
+  // Ze
+  //
   scriptTag.onreadystatechange = function () {
     if (this.readyState == 'complete') onScriptLoad();
   };
